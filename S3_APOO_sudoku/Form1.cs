6a7396665a7b0e9ee8636a13bd352f560fec840f
Yann Villellas <yvillellas@gmail.com>
2022-07-16 23:36:51 +0200

    edit

--- S3_APOO_sudoku/Form1.cs
+++ S3_APOO_sudoku/Form1.cs
@@ -10,11 +10,11 @@ namespace S3_APOO_sudoku
         public Form1()
         {
             InitializeComponent();
-            CreateCells();
-            StartNewGame();
+            Create_Cells();
+            Start_New_Game();
         }
         SudokuCell[,] cells = new SudokuCell[9, 9];
-        private void CreateCells()
+        private void Create_Cells()
         {
             for (int i = 0; i < 9; i++)
             {
@@ -34,18 +34,19 @@ namespace S3_APOO_sudoku
                     cells[i, j].x = i;
                     cells[i, j].y = j;
                     //assign key press event for each cells
-                    cells[i, j].KeyPress += Cell_keyPressed;
+                    cells[i, j].KeyPress += Cell_Key_Pressed;
                     panel1.Controls.Add(cells[i, j]);
                 }
             }
         }
-        private void StartNewGame()
+        private void Start_New_Game()
         {
-            Loadvalues();
+            Load_Values();
+            Show_Random_Values_Hints(15);
         }
-        private void Cell_keyPressed(object? sender, KeyPressEventArgs e)
+        private void Cell_Key_Pressed(object? sender, KeyPressEventArgs e)
         {
-            SudokuCell cell = new();
+            var cell = sender as SudokuCell;
             if (cell.isLocked) //do nothing if the cell is locked
                 return;
             if (int.TryParse(e.KeyChar.ToString(), out int value)) //add the pressed key value in the cell only if it is a number
@@ -57,58 +58,56 @@ namespace S3_APOO_sudoku
                 cell.ForeColor = SystemColors.ControlDarkDark;
             }
         }
-        private void Loadvalues()
+        private void Load_Values()
         {
-            // Clear the values in each cells
+            //clear the values in each cells
             foreach (var cell in cells)
             {
                 cell.value = 0;
                 cell.Clear();
             }
-            // This method will be called recursively 
-            // until it finds suitable values for each cells
-            FindValueForNextCell(0, -1);
+            //this method will be called recursively 
+            //until it finds suitable values for each cells
+            Find_Value_For_Next_Cell(0, -1);
         }
         Random random = new();
-        private bool FindValueForNextCell(int i, int j)
+        private bool Find_Value_For_Next_Cell(int i, int j)
         {
-            // Increment the i and j values to move to the next cell
-            // and if the columsn ends move to the next row
+            //increment the i and j values to move to the next cell
+            //and if the columsn ends move to the next row
             if (++j > 8)
             {
                 j = 0;
 
-                // Exit if the line ends
+                //exit if the line ends
                 if (++i > 8)
                     return true;
             }
             var numsLeft = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             int value;
-            // Find a random and valid number for the cell and go to the next cell 
-            // and check if it can be allocated with another random and valid number
+            //find a random and valid number for the cell and go to the next cell 
+            //and check if it can be allocated with another random and valid number
             do
             {
-                // If there is not numbers left in the list to try next, 
-                // return to the previous cell and allocate it with a different number
+                //if there is not numbers left in the list to try next, 
+                //return to the previous cell and allocate it with a different number
                 if (numsLeft.Count < 1)
                 {
                     cells[i, j].value = 0;
                     return false;
                 }
 
-                // Take a random number from the numbers left in the list
+                //take a random number from the numbers left in the list
                 value = numsLeft[random.Next(0, numsLeft.Count)];
                 cells[i, j].value = value;
 
-                // Remove the allocated value from the list
+                //remove the allocated value from the list
                 numsLeft.Remove(value);
             }
-            while (!IsValidNumber(value, i, j) || !FindValueForNextCell(i, j));
-            // TDO: Remove this line after testing
-            cells[i, j].Text = value.ToString();
+            while (!Is_Valid_Number(value, i, j) || !Find_Value_For_Next_Cell(i, j));
             return true;
         }
-        private bool IsValidNumber(int value, int x, int y)
+        private bool Is_Valid_Number(int value, int x, int y)
         {
             for (int i = 0; i < 9; i++)
             {
@@ -126,6 +125,60 @@ namespace S3_APOO_sudoku
                 }
             }
             return true;
+        }
+        private void Show_Random_Values_Hints(int hintsCount)
+        {
+            // Show value in random cells
+            // The hints count is based on the level player choose
+            for (int i = 0; i < hintsCount; i++)
+            {
+                var rX = random.Next(9);
+                var rY = random.Next(9);
+
+                // Style the hint cells differently and
+                // lock the cell so that player can't edit the value
+                cells[rX, rY].Text = cells[rX, rY].value.ToString();
+                cells[rX, rY].ForeColor = Color.Black;
+                cells[rX, rY].isLocked = true;
+            }
+        }
+        private void Check_Button_Click(object sender, EventArgs e)
+        {
+            var wrongCells = new List<SudokuCell>();
+
+            // Find all the wrong inputs
+            foreach (var cell in cells)
+            {
+                if (!string.Equals(cell.value.ToString(), cell.Text))
+                {
+                    wrongCells.Add(cell);
+                }
+            }
+
+            // Check if the inputs are wrong or the player wins 
+            if (wrongCells.Any())
+            {
+                // Highlight the wrong inputs 
+                wrongCells.ForEach(x => x.ForeColor = Color.Red);
+                MessageBox.Show("Wrong inputs");
+            }
+            else
+            {
+                MessageBox.Show("You Wins");
+            }
+        }
+        private void Clear_Button_Click(object sender, EventArgs e)
+        {
+            foreach (var cell in cells)
+            {
+                // Clear the cell only if it is not locked
+                if (cell.isLocked == false)
+                    cell.Clear();
+            }
+        }
+        private void New_Game_Button_Click(object sender, EventArgs e)
+        {
+            Start_New_Game();
         }
     }
 }
